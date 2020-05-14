a3c9f32c3c2876f1f1dcf6d3a80a00e8e1abfc6d
Marco van Vemden <vanvemden@gmail.com>
2020-05-14 13:40:14 -0700

    added functionality

--- connect4.js
+++ connect4.js
@@ -5,11 +5,11 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,15 +17,29 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for (let cols = 0; cols < WIDTH; cols++) {
+    board.push([]);
+    for (let rows = 0; rows < HEIGHT; rows++) {
+      board[cols].push(null)
+    }
+  }
+  console.log(board);
+
+  // let cols = new Array(WIDTH).fill(null);
+  // for (let i = 0; i < HEIGHT; i++) {
+  //   board.push(cols);
+  // }
+  // board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null));
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const htmlBoard = document.getElementById("board");
 
-  // TODO: add comment for this code
-  var top = document.createElement("tr");
+  // Building top slots to drop the chips
+  let top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
@@ -36,7 +50,7 @@ function makeHtmlBoard() {
   }
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
+  // Building the game board
   for (var y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
     for (var x = 0; x < WIDTH; x++) {
@@ -52,13 +66,25 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for (let y = HEIGHT - 1; y >= 0; y--) {
+    if (board[y][x] === null) {
+      board[y][x] = currPlayer;
+      return y;
+    } else if (y === 0) {
+      // column is full
+      return null;
+    } 
+  }
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+  let td = document.getElementById(`${ y }-${ x }`);
+  let div = document.createElement("div");
+  div.classList.add("piece", `piece-player-${ currPlayer }`);
+  td.appendChild(div);
 }
 
 /** endGame: announce game end */
